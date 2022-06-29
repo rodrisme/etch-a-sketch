2951c456d26a7d6748d6c0d52eb0484b7203ed77
Rodrigo <rodrisme@gmail.com>
2022-06-29 10:48:29 -0300

    Added change grid size button

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
 const container = document.querySelector("#container");
-let size = 16;
+let gridSize = 16;
 
-const createSquares = () => {
+const createSquare = () => {
   const createSquare = document.createElement("div");
   createSquare.setAttribute("id", "square");
   container.appendChild(createSquare);
+  createSquare.style.height = `${600 / gridSize}px`;
+  createSquare.style.width = `${600 / gridSize}px`;
+};
+const createGrid = () => {
+  for (let i = 0; i < gridSize ** 2; i++) {
+    createSquare();
+  }
 };
 
-for (let i = 0; i < size ** 2; i++) {
-  createSquares();
-}
-
-const test = document.querySelectorAll("div#square");
-test.forEach((div) => {
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "black";
+const mouseOver = () => {
+  const square = document.querySelectorAll("div#square");
+  square.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    });
   });
-});
+};
+
+createGrid();
+mouseOver();
+
+const removeGrid = () => {
+  for (let i = 0; i < gridSize ** 2; i++) {
+    document.getElementById("square").remove();
+  }
+};
 
 const button = document.querySelector("button");
 
-button.addEventListener("click", grid);
+button.addEventListener("click", () => {
+  removeGrid();
+  changeGridSize();
+  mouseOver();
+});
 
-function grid() {
-  size = prompt("Enter new grid size");
-}
+const changeGridSize = () => {
+  gridSize = parseInt(
+    prompt("Enter a number between 16 and 100 for a new grid")
+  );
+  while (isNaN(gridSize) || gridSize < 16 || gridSize > 100) {
+    if ((gridSize = " ")) {
+      gridSize = 16;
+      break;
+    } else {
+      gridSize = parseInt(
+        prompt("Enter a number between 16 and 100 for a new grid")
+      );
+    }
+  }
+  createGrid();
+};
